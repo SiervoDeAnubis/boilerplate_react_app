6910c5fae099c45e643abea687137a46f318174e
César Olayo Espíritu <gb_cespiritu@idcontractors.net>
2020-01-14 13:30:36 -0600

    Boilerplate for React App

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "react-dom";
+import styles from "./styles/global.css";
+
+const App = () => {
+  return (
+    <div className={`${styles.home}`}>
+      <h1>a lot of configuration</h1>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader", options: { modules: true } }
+        ]
+      }
+    ]
+  }
+};
